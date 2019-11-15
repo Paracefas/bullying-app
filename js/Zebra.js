adaac3db6448b61347ebf43c7899665c854ad792
Navi099 <ivanlus827@gmail.com>
2019-11-14 23:25:13 -0300

    Lus

---
+++ js/Zebra.js
@@ -0,0 +1,29 @@
+function comprobar()
+{
+var pregunta1= document.Quiz.preg1.value;
+var contador= 0;
+
+	if(pregunta1=="Se ríe de su compañero")
+	{
+		contador= contador + 1;
+	}
+
+switch(contador)
+{
+	case 0:
+	$.Zebra_Dialog('<strong>Mal</strong>, no respondiste la pregunta correctamente', {
+    'type':     'error',
+    'title':    'Quiz resultado'
+});
+	break;
+case 1:
+	$.Zebra_Dialog('<strong>Muy Bien</strong>, respondiste la pregunta correctamente', {
+    'type':     'confirmation',
+    'title':    'Quiz resultado'
+});
+	break;
+
+}
+
+}
+
---
+++ js/Zebra3.js
@@ -0,0 +1,29 @@
+function comprobar()
+{
+var pregunta3= document.Quiz.preg3.value;
+var contador= 0;
+
+	if(pregunta3=="Se confundió")
+	{
+		contador= contador + 1;
+	}
+
+switch(contador)
+{
+	case 0:
+	$.Zebra_Dialog('<strong>Mal</strong>, no respondiste la pregunta correctamente', {
+    'type':     'error',
+    'title':    'Quiz resultado'
+});
+	break;
+case 1:
+	$.Zebra_Dialog('<strong>Muy Bien</strong>, respondiste la pregunta correctamente', {
+    'type':     'confirmation',
+    'title':    'Quiz resultado'
+});
+	break;
+
+}
+
+}
+
